306720d830768d559f2a58f44d29048f53ad489b
Maxim <maximbear06@gmail.com>
2026-06-24 22:27:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+PRODUCT_URL = "https://www.auchan.ru/product/hleb-kolomenskoe-baton-nareznoy-vysshiy-sort-v-narezku-400-g/"
+PRODUCT_NAME = "Батон Коломенское нарезной 400г"
+DB_NAME = "product_prices.db"
+CHECK_INTERVAL_SECONDS = 400
